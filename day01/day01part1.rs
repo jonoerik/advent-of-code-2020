b787fd8a3e36a21d570134b3ad18b57538e46940
jonoerik <jonoerik@fastmail.fm>
2022-09-06 18:14:10 +1000

    Factor out reading of input data.

--- day01/day01part1.rs
+++ day01/day01part1.rs
@@ -10,8 +10,27 @@ struct Args {
     input_path: String,
 }
 
+/// From the file at path, read a list of integers (one per line), and return them
+/// as a Vec<u32>.
+fn load_input(path: String) -> Result<Vec<u32>, String> {
+    let file_string = match fs::read_to_string(path) {
+        Ok(s) => s,
+        Err(err) => {
+            return Err(format!("{}.", err));
+        },
+    };
+    match file_string.lines().map(|x| x.parse::<u32>()).collect() {
+        Ok(xs) => return Ok(xs),
+        Err(err) => {
+            return Err(format!("{}.", err));
+        },
+    };
+}
+
+/// Value to which two elements of the input must sum.
 const TARGET_TOTAL: u32 = 2020;
 
+/// From the input list of values, find two that sum to TARGET_TOTAL, and return their product.
 fn find_match(input: Vec<u32>) -> Result<u32, String> {
     match input.into_iter().combinations(2).find(|x| x.into_iter().sum::<u32>() == TARGET_TOTAL) {
         Some(x) => Ok(x.into_iter().product()),
@@ -21,31 +40,23 @@ fn find_match(input: Vec<u32>) -> Result<u32, String> {
 
 fn main() -> ExitCode {
     let args = Args::parse();
-
-    let file_string = match fs::read_to_string(args.input_path) {
-        Ok(s) => s,
-        Err(err) => {
-            eprintln!("IO Error: {}.", err);
-            return ExitCode::FAILURE
-        },
-    };
-    let input = match file_string.lines().map(|x| x.parse::<u32>()).collect() {
-        Ok(xs) => xs,
-        Err(err) => {
-            eprintln!("Error: {}.", err);
+    let input = match load_input(args.input_path){
+        Ok(input) => input,
+        Err(msg) => {
+            eprintln!("Error: {}.", msg);
             return ExitCode::FAILURE;
         },
     };
     match find_match(input) {
         Ok(x) => {
             println!("{}", x);
-            ExitCode::SUCCESS
+            return ExitCode::SUCCESS
         },
         Err(msg) => {
             eprintln!("Error: {}.", msg);
-            ExitCode::FAILURE
+            return ExitCode::FAILURE
         },
-    }
+    };
 }
 
 //TODO add tests
