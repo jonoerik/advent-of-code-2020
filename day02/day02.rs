8e55abb754d0fcc1e609bbe333f6115a55a03d65
jonoerik <jonoerik@fastmail.fm>
2022-09-07 10:02:15 +1000

    Add framework for part2 solution. Add comments.

--- day02/day02.rs
+++ day02/day02.rs
@@ -12,12 +12,13 @@ use regex::Regex;
 #[clap()]
 struct Args {
     input_path: PathBuf,
-    //#[clap(short = '1', long, action)]
-    //part1: bool,
-    //#[clap(short = '2', long, action)]
-    //part2: bool,
+    #[clap(short = '1', long, action)]
+    part1: bool,
+    #[clap(short = '2', long, action)]
+    part2: bool,
 }
 
+/// A single element of the puzzle input.
 struct InputEntry {
     required_min: usize,
     required_max: usize,
@@ -25,6 +26,7 @@ struct InputEntry {
     password: String,
 }
 
+/// From the provided filesystem path, load the puzzle input.
 fn load_input(path: &Path) -> Result<Vec<InputEntry>, String> {
     let input_re = Regex::new(r"^(?P<min>[0-9]+)-(?P<max>[0-9]+) (?P<char>.): (?P<password>.+)$").unwrap();
     let input_file = match fs::read_to_string(path) {
@@ -46,12 +48,25 @@ fn load_input(path: &Path) -> Result<Vec<InputEntry>, String> {
     }).collect();
 }
 
-fn check_entries(entries: &Vec<InputEntry>) -> usize {
-    entries.into_iter().map(|entry| if check_entry(entry) {1usize} else {0usize}).sum()
+/// Return a count of the number of provided entries that match their respective part1 password policies.
+fn check_entries_p1(entries: &Vec<InputEntry>) -> usize {
+    entries.into_iter().map(|entry| if check_entry_p1(entry) {1usize} else {0usize}).sum()
 }
 
-fn check_entry(entry: &InputEntry) -> bool {
+/// Return true if the single provided entry is valid according to its own part1 password policy, false otherwise.
+fn check_entry_p1(entry: &InputEntry) -> bool {
     (entry.required_min..=entry.required_max).contains(&entry.password.chars().filter(|c| *c == entry.required_char).count())
+
+}
+
+/// Return a count of the number of provided entries that match their respective part2 password policies.
+fn check_entries_p2(entries: &Vec<InputEntry>) -> usize {
+    entries.into_iter().map(|entry| if check_entry_p2(entry) {1usize} else {0usize}).sum()
+}
+
+/// Return true if the single provided entry is valid according to its own part2 password policy, false otherwise.
+fn check_entry_p2(entry: &InputEntry) -> bool {
+    unimplemented!();
 }
 
 fn main() -> ExitCode {
@@ -63,16 +78,36 @@ fn main() -> ExitCode {
             return ExitCode::FAILURE;
         }
     };
-    println!("{}", check_entries(&input));
+
+    let check_fn = match (args.part1, args.part2) {
+        (true, false) => check_entries_p1,
+        (false, true) => check_entries_p2,
+        _ => {
+            eprintln!("Error: must specify exactly one of --part1 or --part2.");
+            return ExitCode::FAILURE;
+        },
+    };
+
+    println!("{}", check_fn(&input));
     return ExitCode::SUCCESS;
 }
 
 #[datatest::files("data", {
     input in r"^data/sample([0-9]+)$",
-    answer = r"data/sample${1}.answer",
+    answer = r"data/sample${1}.answer1",
 })]
-fn tests(input: &Path, answer: &str) {
+fn tests_p1(input: &Path, answer: &str) {
     let input = load_input(input).unwrap();
     let answer = answer.parse::<usize>().unwrap();
-    assert_eq!(check_entries(&input), answer);
+    assert_eq!(check_entries_p1(&input), answer);
+}
+
+#[datatest::files("data", {
+    input in r"^data/sample([0-9]+)$",
+    answer = r"data/sample${1}.answer2",
+})]
+fn tests_p2(input: &Path, answer: &str) {
+    let input = load_input(input).unwrap();
+    let answer = answer.parse::<usize>().unwrap();
+    assert_eq!(check_entries_p2(&input), answer);
 }
